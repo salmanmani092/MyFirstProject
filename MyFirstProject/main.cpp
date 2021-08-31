0ba1347bf5e09567d8b4914d96bd5ea77598637f
salmanmani092 <70169067+salmanmani092@users.noreply.github.com>
2021-08-30 22:55:21 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,14 @@
+/*
+Muhammad Salman
+Fall 2021
+Lab 1
+This lab is to setup an account on github, creat and uplaod a repo to github. Also to start main functions and include liberary.
+*/
+
+#include <iostream>
+int main()
+{
+	std::cout << "Hello World!";
+	std::cout << "My favorite color is green.";
+	return 0;
+}
